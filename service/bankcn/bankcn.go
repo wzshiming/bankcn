4b76b21dd44aaa493d1129986be9533b705088a7
wzshiming <wzshiming@foxmail.com>
2019-03-27 18:07:54 +0800

    更新注释

--- service/bankcn/bankcn.go
+++ service/bankcn/bankcn.go
@@ -13,17 +13,22 @@ func NewBankcnService() (*BankcnService, error) {
 	return &BankcnService{}, nil
 }
 
-// Banks #route:"GET /banks"# 获取某地区的银行数据
+// Banks #route:"GET /banks"# 获取全部的银行
 func (s *BankcnService) Banks() (banks map[string]string, err error) {
 	return bankcn.BankMap, nil
 }
 
-// Banks #route:"GET /banks/{bank_id}.jpg"# 获取某地区的银行数据
-func (s *BankcnService) BanksJPG(bankID string /* #name:"bank_id"# */) (file []byte /* #content:"image/jpeg"# */, err error /* #code:"404"# */) {
-	return bankcn.Asset("icon/" + bankID + ".jpg")
+// Banks #route:"GET /banks/{bank}.jpg"# 获取某地区的银行图标
+func (s *BankcnService) BanksJPG(bank string /* #name:"bank"# */) (file []byte /* #content:"image/jpeg"# */, err error /* #code:"404"# */) {
+	return bankcn.Asset("icon/" + bank + ".jpg")
 }
 
-// Get #route:"GET /{bank_id}/{area_id}"# 获取某地区的银行数据
-func (s *BankcnService) Get(bankID /* #name:"bank_id"# */, areaID string /* #name:"area_id"# */) (banks []*bankcn.Bank, err error) {
-	return bankcn.Get(bankID, areaID), nil
+// Get #route:"GET /{bank}/{area_id}"# 获取某地区的银行数据
+func (s *BankcnService) Get(bank /* #name:"bank"# */, areaID string /* #name:"area_id"# */) (banks []*bankcn.Bank, err error) {
+	return bankcn.Get(bank, areaID), nil
+}
+
+// Verify #route:"GET /{bank_id}"# 获取银行卡属性
+func (s *BankcnService) Verify(bankID string /* #name:"bank_id"# */) (valid *bankcn.Valid, err error) {
+	return bankcn.Verify(bankID)
 }
