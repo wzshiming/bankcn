549aecd269881e0545697d7f6dd83abfb5473a45
wzshiming <wzshiming@foxmail.com>
2019-03-29 11:22:42 +0800

    regenerate

--- cmd/bankcn/router_gen.go
+++ cmd/bankcn/router_gen.go
@@ -97,6 +97,15 @@ func _requestPathBankID(w http.ResponseWriter, r *http.Request) (_bankID string,
 	return
 }
 
+// _requestQueryFilter Parsing the query for of filter
+func _requestQueryFilter(w http.ResponseWriter, r *http.Request) (_filter string, err error) {
+
+	var _raw_filter = r.URL.Query().Get("filter")
+	_filter = string(_raw_filter)
+
+	return
+}
+
 // _operationGetBankcnBankIDBankID Is the route of Verify
 func _operationGetBankcnBankIDBankID(s *githubComWzshimingBankcnServiceBankcn.BankcnService, w http.ResponseWriter, r *http.Request) {
 
@@ -154,6 +163,7 @@ func _operationGetBankcnBanksBankAreaID(s *githubComWzshimingBankcnServiceBankcn
 	var err error
 	var _bank string
 	var _areaID string
+	var _filter string
 	var _banks []*githubComWzshimingBankcn.Bank
 
 	// Parsing bank.
@@ -168,8 +178,14 @@ func _operationGetBankcnBanksBankAreaID(s *githubComWzshimingBankcnServiceBankcn
 		return
 	}
 
+	// Parsing filter.
+	_filter, err = _requestQueryFilter(w, r)
+	if err != nil {
+		return
+	}
+
 	// Call github.com/wzshiming/bankcn/service/bankcn BankcnService.Get.
-	_banks, err = s.Get(_bank, _areaID)
+	_banks, err = s.Get(_bank, _areaID, _filter)
 
 	// Response code 200 OK for banks.
 	if _banks != nil {
@@ -396,6 +412,7 @@ paths:
       parameters:
       - $ref: '#/components/parameters/bank'
       - $ref: '#/components/parameters/area_id'
+      - $ref: '#/components/parameters/filter'
       responses:
         "200":
           description: Response code is 200
@@ -516,11 +533,16 @@ components:
       required: true
       schema:
         type: string
+    filter:
+      name: filter
+      in: query
+      schema:
+        type: string
 tags:
 - name: BankcnService
   description: 'BankcnService #path:"/bankcn/"#'
 `)
-var OpenAPI4JSON = []byte(`{"openapi":"3.0.1","info":{"title":"OpenAPI Demo","description":"Automatically generated","contact":{"name":"wzshiming","url":"https://github.com/wzshiming/gen"},"version":"0.0.1"},"servers":[{"url":"/"},{"url":"{scheme}{host}{port}{path}","variables":{"host":{"enum":["localhost"],"default":"localhost"},"path":{"enum":["/"],"default":"/"},"port":{"enum":[""],"default":""},"scheme":{"enum":["http://","https://"],"default":"http://"}}}],"paths":{"/bankcn/bank_id/{bank_id}":{"get":{"tags":["BankcnService"],"summary":"Verify #route:\"GET /bank_id/{bank_id}\"# 获取银行卡号的 所属银行 以及 类型","description":"Verify #route:\"GET /bank_id/{bank_id}\"# 获取银行卡号的 所属银行 以及 类型","parameters":[{"$ref":"#/components/parameters/bank_id"}],"responses":{"200":{"description":"Response code is 200","content":{"application/json":{"schema":{"$ref":"#/components/schemas/Valid"}}}},"400":{"description":"Response code is 400","content":{"text/plain":{"schema":{"type":"string","format":"error"}}}}}}},"/bankcn/banks":{"get":{"tags":["BankcnService"],"summary":"Banks #route:\"GET /banks\"# 获取全部的银行类型的列表","description":"Banks #route:\"GET /banks\"# 获取全部的银行类型的列表","responses":{"200":{"description":"Response code is 200","content":{"application/json":{"schema":{"type":"array","items":{"$ref":"#/components/schemas/BankItem"}}}}},"400":{"description":"Response code is 400","content":{"text/plain":{"schema":{"type":"string","format":"error"}}}}}}},"/bankcn/banks/{bank}.jpg":{"get":{"tags":["BankcnService"],"summary":"Banks #route:\"GET /banks/{bank}.jpg\"# 获取某银行图标","description":"Banks #route:\"GET /banks/{bank}.jpg\"# 获取某银行图标","parameters":[{"$ref":"#/components/parameters/bank"}],"responses":{"200":{"description":"#content:\"image/jpeg\"#","content":{"image/jpeg":{}}},"404":{"description":"#code:\"404\"#","content":{"text/plain":{"schema":{"type":"string","format":"error"}}}}}}},"/bankcn/banks/{bank}/{area_id}":{"get":{"tags":["BankcnService"],"summary":"Get #route:\"GET /banks/{bank}/{area_id}\"# 获取某地区的银行数据","description":"Get #route:\"GET /banks/{bank}/{area_id}\"# 获取某地区的银行数据","parameters":[{"$ref":"#/components/parameters/bank"},{"$ref":"#/components/parameters/area_id"}],"responses":{"200":{"description":"Response code is 200","content":{"application/json":{"schema":{"type":"array","items":{"$ref":"#/components/schemas/Bank"}}}}},"400":{"description":"Response code is 400","content":{"text/plain":{"schema":{"type":"string","format":"error"}}}}}}}},"components":{"schemas":{"Bank":{"type":"object","properties":{"address":{"type":"string","description":"联系地址"},"area_id":{"type":"string","description":"所在区域代号"},"bank":{"type":"string","description":"银行标识符"},"bank_union_id":{"type":"string","description":"银行联行号"},"name":{"type":"string","description":"银行名字"},"phone":{"type":"string","description":"联系电话"}}},"BankItem":{"type":"object","properties":{"bank":{"type":"string","description":"银行代号"},"bank_name":{"type":"string","description":"银行名"}}},"Valid":{"type":"object","properties":{"bank":{"type":"string","description":"银行卡所属银行"},"bank_id_type":{"type":"string","description":"银行卡 类型"}}}},"responses":{"bankItems":{"description":"Response code is 200","content":{"application/json":{"schema":{"type":"array","items":{"$ref":"#/components/schemas/BankItem"}}}}},"banks":{"description":"Response code is 200","content":{"application/json":{"schema":{"type":"array","items":{"$ref":"#/components/schemas/Bank"}}}}},"err":{"description":"Response code is 400","content":{"text/plain":{"schema":{"type":"string","format":"error"}}}},"err.1":{"description":"#code:\"404\"#","content":{"text/plain":{"schema":{"type":"string","format":"error"}}}},"file":{"description":"#content:\"image/jpeg\"#","content":{"image/jpeg":{}}},"valid":{"description":"Response code is 200","content":{"application/json":{"schema":{"$ref":"#/components/schemas/Valid"}}}}},"parameters":{"area_id":{"name":"area_id","in":"path","description":"#name:\"area_id\"#","required":true,"schema":{"type":"string"}},"bank":{"name":"bank","in":"path","description":"#name:\"bank\"#","required":true,"schema":{"type":"string"}},"bank_id":{"name":"bank_id","in":"path","description":"#name:\"bank_id\"#","required":true,"schema":{"type":"string"}}}},"tags":[{"name":"BankcnService","description":"BankcnService #path:\"/bankcn/\"#"}]}`)
+var OpenAPI4JSON = []byte(`{"openapi":"3.0.1","info":{"title":"OpenAPI Demo","description":"Automatically generated","contact":{"name":"wzshiming","url":"https://github.com/wzshiming/gen"},"version":"0.0.1"},"servers":[{"url":"/"},{"url":"{scheme}{host}{port}{path}","variables":{"host":{"enum":["localhost"],"default":"localhost"},"path":{"enum":["/"],"default":"/"},"port":{"enum":[""],"default":""},"scheme":{"enum":["http://","https://"],"default":"http://"}}}],"paths":{"/bankcn/bank_id/{bank_id}":{"get":{"tags":["BankcnService"],"summary":"Verify #route:\"GET /bank_id/{bank_id}\"# 获取银行卡号的 所属银行 以及 类型","description":"Verify #route:\"GET /bank_id/{bank_id}\"# 获取银行卡号的 所属银行 以及 类型","parameters":[{"$ref":"#/components/parameters/bank_id"}],"responses":{"200":{"description":"Response code is 200","content":{"application/json":{"schema":{"$ref":"#/components/schemas/Valid"}}}},"400":{"description":"Response code is 400","content":{"text/plain":{"schema":{"type":"string","format":"error"}}}}}}},"/bankcn/banks":{"get":{"tags":["BankcnService"],"summary":"Banks #route:\"GET /banks\"# 获取全部的银行类型的列表","description":"Banks #route:\"GET /banks\"# 获取全部的银行类型的列表","responses":{"200":{"description":"Response code is 200","content":{"application/json":{"schema":{"type":"array","items":{"$ref":"#/components/schemas/BankItem"}}}}},"400":{"description":"Response code is 400","content":{"text/plain":{"schema":{"type":"string","format":"error"}}}}}}},"/bankcn/banks/{bank}.jpg":{"get":{"tags":["BankcnService"],"summary":"Banks #route:\"GET /banks/{bank}.jpg\"# 获取某银行图标","description":"Banks #route:\"GET /banks/{bank}.jpg\"# 获取某银行图标","parameters":[{"$ref":"#/components/parameters/bank"}],"responses":{"200":{"description":"#content:\"image/jpeg\"#","content":{"image/jpeg":{}}},"404":{"description":"#code:\"404\"#","content":{"text/plain":{"schema":{"type":"string","format":"error"}}}}}}},"/bankcn/banks/{bank}/{area_id}":{"get":{"tags":["BankcnService"],"summary":"Get #route:\"GET /banks/{bank}/{area_id}\"# 获取某地区的银行数据","description":"Get #route:\"GET /banks/{bank}/{area_id}\"# 获取某地区的银行数据","parameters":[{"$ref":"#/components/parameters/bank"},{"$ref":"#/components/parameters/area_id"},{"$ref":"#/components/parameters/filter"}],"responses":{"200":{"description":"Response code is 200","content":{"application/json":{"schema":{"type":"array","items":{"$ref":"#/components/schemas/Bank"}}}}},"400":{"description":"Response code is 400","content":{"text/plain":{"schema":{"type":"string","format":"error"}}}}}}}},"components":{"schemas":{"Bank":{"type":"object","properties":{"address":{"type":"string","description":"联系地址"},"area_id":{"type":"string","description":"所在区域代号"},"bank":{"type":"string","description":"银行标识符"},"bank_union_id":{"type":"string","description":"银行联行号"},"name":{"type":"string","description":"银行名字"},"phone":{"type":"string","description":"联系电话"}}},"BankItem":{"type":"object","properties":{"bank":{"type":"string","description":"银行代号"},"bank_name":{"type":"string","description":"银行名"}}},"Valid":{"type":"object","properties":{"bank":{"type":"string","description":"银行卡所属银行"},"bank_id_type":{"type":"string","description":"银行卡 类型"}}}},"responses":{"bankItems":{"description":"Response code is 200","content":{"application/json":{"schema":{"type":"array","items":{"$ref":"#/components/schemas/BankItem"}}}}},"banks":{"description":"Response code is 200","content":{"application/json":{"schema":{"type":"array","items":{"$ref":"#/components/schemas/Bank"}}}}},"err":{"description":"Response code is 400","content":{"text/plain":{"schema":{"type":"string","format":"error"}}}},"err.1":{"description":"#code:\"404\"#","content":{"text/plain":{"schema":{"type":"string","format":"error"}}}},"file":{"description":"#content:\"image/jpeg\"#","content":{"image/jpeg":{}}},"valid":{"description":"Response code is 200","content":{"application/json":{"schema":{"$ref":"#/components/schemas/Valid"}}}}},"parameters":{"area_id":{"name":"area_id","in":"path","description":"#name:\"area_id\"#","required":true,"schema":{"type":"string"}},"bank":{"name":"bank","in":"path","description":"#name:\"bank\"#","required":true,"schema":{"type":"string"}},"bank_id":{"name":"bank_id","in":"path","description":"#name:\"bank_id\"#","required":true,"schema":{"type":"string"}},"filter":{"name":"filter","in":"query","schema":{"type":"string"}}}},"tags":[{"name":"BankcnService","description":"BankcnService #path:\"/bankcn/\"#"}]}`)
 
 // RouteOpenAPI
 func RouteOpenAPI(router *mux.Router) *mux.Router {
